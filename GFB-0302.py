b17da59540316835d34dc7542b29c0efdcc6134c
ssghost <ssprof0@gmail.com>
2022-11-18 15:55:10 +0800

    0302 bug fixed

--- GFB-0302.py
+++ GFB-0302.py
@@ -5,9 +5,9 @@ class Solution:
         ptr = [0,0]
         passed = [[0,0]]
         spade = True
-        cache, cachestep = [], []
+        cache, cachestep, cachespade = [], [], []
         step = 1
-        def findnext(ptr: List[int], step: int) -> Tuple[List[int], int]:
+        def findnext(ptr: List[int], step: int, spade: bool) -> Tuple[List[int], int, bool]:
             possible = []
             udlr = [[ptr[0]-1,ptr[1]],[ptr[0]+1,ptr[1]],[ptr[0],ptr[1]-1],[ptr[0],ptr[1]+1]]
             for i in range(4):
@@ -19,16 +19,17 @@ class Solution:
                         if M[udlr[i][0]][udlr[i][1]] == 0:
                             possible.append(udlr[i])
             if len(possible) == 0:
-                return cache.pop(), (cachestep.pop())+1 
+                return cache.pop(), (cachestep.pop())+1, cachespade.pop()
             if len(possible) == 1:
-                return possible[0], step+1
+                return possible[0], step+1, spade
             if len(possible) > 1:
                 out = possible.pop()
                 cache.extend(possible)
                 cachestep.extend([step]*len(possible))
-                return out, step+1
+                cachespade.extend([spade]*len(possible))
+                return out, step+1, spade
         while ptr[0] < x-1 or ptr[1] < y-1:
-            ptr, step = findnext(ptr, step)
+            ptr, step, spade = findnext(ptr, step, spade)
             if M[ptr[0]][ptr[1]] == 1:
                 spade = False
             passed.append(ptr)
